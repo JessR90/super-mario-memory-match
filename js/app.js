41287cf0f9428918a529873430cb82427599dd2e
JessR90 <jessica.roias@yahoo.com>
2024-07-20 18:45:42 -0400

    added favicon, fixed audio and picture files, fixed html h1 into the body

--- js/app.js
+++ js/app.js
@@ -13,60 +13,60 @@ console.log("Super Mario Match Game");
 
 
 /*-------------------------------- Constants --------------------------------*/
-const backgroundSound = new Audio("assets/audio/Background N Spade.mp3")
-const cardMatch = new Audio("assets/audio/Card Match Match.wav")
-const cardMiss = new Audio("assets/audio/Card Miss No match.wav")
-const cardSelect = new Audio("assets/audio/Card Select Item Inventory Open or Close.wav")
-const chestMatch = new Audio("assets/audio/Chest match 1-Up Match.wav")
-const gameOver = new Audio("assets/audio/If lost Game Over.mp3")
-const gameWon = new Audio("assets/audio/Game Won.mp3")
+const backgroundMusic = new Audio("assets/audio/backgroundmusic.mp3")
+const cardMatch = new Audio("assets/audio/cardmatch.mp3")
+const cardMiss = new Audio("assets/audio/cardmiss.mp3")
+const cardSelect = new Audio("assets/audio/cardselect.mp3")
+const chestMatch = new Audio("assets/audio/chestmatch.mp3")
+const gameOver = new Audio("assets/audio/gameover.mp3")
+const gameWon = new Audio("assets/audio/gamewon.mp3")
 
 
 
 
 /*---------------------------- Variables (state) ----------------------------*/
-let cardPlay, win, loss
-let cardsLeft = []
-let turn = 1;
+// let cardPlay, win, loss
+// let cardsLeft = []
+// let turn = 1;
 
 
 /*------------------------ Cached Element References ------------------------*/
-const resetButton = document.getElementById("reset")
-// console.log(reset);
+// const resetButton = document.getElementById("reset")
+// // console.log(reset);
 
-const cards = document.querySelectorAll(".cards")
-// console.log(cards);
+// const cards = document.querySelectorAll(".cards")
+// // console.log(cards);
 
 
 
 /*-------------------------------- Functions --------------------------------*/
-init()
+// init()
 
-function init() {
-    cardPlay = ["", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""]
-    match = "yay"
-    miss = "nay"
-    render()
-}
+// function init() {
+//     cardPlay = ["", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""]
+//     match = "yay"
+//     miss = "nay"
+//     render()
+// }
 
-function render() {
-    updateCardPlay()
-}
+// function render() {
+//     updateCardPlay()
+// }
 
-function updateCardPlay() {
-    cardPlay.forEach((selection, idx) => {
-        if (selection === "match") {
-        //  
-        }else (selection === "miss")
-        // 
-    });
-}
+// function updateCardPlay() {
+//     cardPlay.forEach((selection, idx) => {
+//         if (selection === "match") {
+//         //  
+//         }else (selection === "miss")
+//         // 
+//     });
+// }
 
 
 
 
 /*----------------------------- Event Listeners -----------------------------*/
-resetButton.addEventListener("click", resetGame)
+// resetButton.addEventListener("click", resetGame)
 
 // cards.forEach((cardSelect) => {
 //     cardSelect.addEventListener("click", handleClick)  
