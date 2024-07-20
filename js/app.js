0b95664ef1bbf6d9ea69e090dbd28a48c295eabc
JessR90 <jessica.roias@yahoo.com>
2024-07-19 20:23:39 -0400

    fixed audio issue and confirmed all audio works

--- js/app.js
+++ js/app.js
@@ -13,36 +13,42 @@ console.log("Super Mario Match Game");
 
 
 /*-------------------------------- Constants --------------------------------*/
-
+const backgroundSound = new Audio("assets/audio/Background N Spade.mp3")
+const cardMatch = new Audio("assets/audio/Card Match Match.wav")
+const cardMiss = new Audio("assets/audio/Card Miss No match.wav")
+const cardSelect = new Audio("assets/audio/Card Select Item Inventory Open or Close.wav")
+const chestMatch = new Audio("assets/audio/Chest match 1-Up Match.wav")
+const gameOver = new Audio("assets/audio/If lost Game Over.mp3")
+const gameWon = new Audio("assets/audio/Game Won.mp3")
 
 
 /*---------------------------- Variables (state) ----------------------------*/
-let board, win, loss
+// let board, win, loss
 
 
 /*------------------------ Cached Element References ------------------------*/
-const resetBtnEl = document.getElementById("reset")
-// console.log(reset);
-const 
+// const resetBtnEl = document.getElementById("reset")
+// // console.log(reset);
+// const 
 
 
 /*-------------------------------- Functions --------------------------------*/
-init()
+// init()
 
-function init() {
-    board = ["", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""]
-    render()
-}
+// function init() {
+//     board = ["", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""]
+//     render()
+// }
 
-function render() {
-    updateBoard()
-}
+// function render() {
+//     updateBoard()
+// }
 
 
 
 
 /*----------------------------- Event Listeners -----------------------------*/
-resetBtnEl.addEventListener("click", init)
+// resetBtnEl.addEventListener("click", init)
 
 
 
